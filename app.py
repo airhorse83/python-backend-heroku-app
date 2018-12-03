cb051269f3a790cbc475b6c31dcad14690216c49
airhorse83 <airhorse83@yahoo.com>
2018-12-03 11:43:07 -0700

    added db and api endpoint

--- app.py
+++ app.py
@@ -4,13 +4,41 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ""
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://vjkhdjrydbigxj:260c8f99adf9b38164f8f3a817e97d9a53456f70fac1bd5984206071f3546ce7@ec2-184-72-239-186.compute-1.amazonaws.com:5432/dcg65lv1r0cnn6"
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
+
+
 @app.route('/')
 def home():
-    return "<h1>welcome!</h1>"
+    return render_template('home.html')
+
+@app.route('/collections', methods=['POST'])
+def collections():
+    email = None
+    if request.method == 'POST':
+        email = request.form['email']   
+        reg = User(email)
+        db.session.add(reg)
+        db.session.commit()
+        return render_template('success.html')
+    return render_template('home.html')    
+
+@app.route('/return_emails', methods=['GET'])
+def return_emails():
+    all_emails = db.session.query(User.email).all()
+    return jsonify(all_emails)    
 
 if __name__ == '__main__':
     app.debug = True
